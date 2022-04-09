095af9a3062f3faa7178ad4bd6717fa82da51350
TANGFENGNING <102674964+TANGFENGNING@users.noreply.github.com>
2022-04-09 10:11:22 +0800

    Initial commit

---
+++ test_da/test_today.py
@@ -0,0 +1,7 @@
+while (1):
+    num = int(input("请输入被测数字:"))
+
+    def solution(num):
+            pass
+
+    print(num)
---
+++ venv/1.py
@@ -0,0 +1,55 @@
+# # int_a=0
+# # while(int_a<10):
+# #     int_a=int_a+1
+# #     a='abcdefghikjkjiuow'
+# #     print(a,int_a,a[int_a-1])
+# # print('asdniwni is  %d'%1)
+# a=1
+# if a:
+#     a=a-1
+#     print(a)
+#     if a>-10:
+#         b = a + 1
+#     else :
+#         quit()
+# a=0
+# for i in range(1,101):
+#     if i%2==0:
+#         a=a+i
+#         print(a)
+# import random
+#
+# computer_num=random.randint(1,100)
+#     print('输入一个数字'，)
+# set1=set()
+# set1.add(1)
+# set1.add(2)
+# set1.add("iasndyuwbu")
+#
+#
+# set1.add("iasndyuwbu")
+# print(set1)
+# def inc(x):
+#     return x+1
+# def test_answer():
+#     assert inc(4)==5
+
+
+#
+#
+# def solution(num):
+#         numb1=(num)
+#         x_list=[]
+#         X=list(map(int,str(numb1)))
+#         for i in range (len(X)-1):
+#            for j in range (i+1,len(X)):
+#
+#               # x_list.append(X[j])
+#               # if x_list==x_list[::-1]:
+#               #    print(x_list)
+#
+#
+#
+# solution(10801)
+
+
